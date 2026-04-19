88d9d0b93961392a6ead84ac5148bd1ab2d299b8
Foxie <167193515+SancandiGA@users.noreply.github.com>
2026-04-19 19:37:25 +0200

    Update ServerMaiuscolo.java

--- Esercitazione 04/Esercizio b/ServerMaiuscolo.java
+++ Esercitazione 04/Esercizio b/ServerMaiuscolo.java
@@ -4,18 +4,17 @@ public class ServerMaiuscolo {
     public static void main(String[] args) throws Exception {
         DatagramSocket serverSocket = new DatagramSocket(9876);
         byte[] receiveData = new byte[1024];
-        
+
         while(true) {
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
             serverSocket.receive(receivePacket);
             
             String messaggio = new String(receivePacket.getData(), 0, receivePacket.getLength());
             String risposta = messaggio.toUpperCase();
+            byte[] sendData = risposta.getBytes();
             
             InetAddress IPAddress = receivePacket.getAddress();
             int port = receivePacket.getPort();
-            byte[] sendData = risposta.getBytes();
-            
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
             serverSocket.send(sendPacket);
         }
